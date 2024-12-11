9c0d21e7c18730902da5c94b5ba5a0bc3a528b2f
Simone-Domenici <domenici.simone00@gmail.com>
2024-12-11 16:05:51 +0100

    ADD PostsContext

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
 import DefaultLayout from './layouts/DefaultLayout';
@@ -8,25 +8,47 @@ import BlankLayout from './layouts/BlankLayout';
 import NotFound from './pages/NotFound';
 import PostDetail from './pages/PostDetail';
 import CreatePost from './pages/posts/CreatePost';
+import PostsContext from './contexts/PostsContext';
+import axios from 'axios';
 
 function App(){
+
+  const [posts, setPosts] = useState([]);
+  const [categories, setCategories] = useState([]);
+
+  // Recupera i post
+  useEffect(() => {
+    axios.get("http://localhost:3000/posts")
+      .then(res => setPosts(res.data))
+      .catch(err => console.error(err));
+  }, []);
+
+  // Recupera le categorie
+  useEffect(() => {
+    axios.get("http://localhost:3000/posts/categories")
+      .then(res => setCategories(res.data))
+      .catch(err => console.error(err));
+  }, []);
+
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route element={<DefaultLayout />}>
-          <Route path="/" element={<Home />} />
-          <Route path='/about' element={<About />} />
-          <Route path='/posts'>
-            <Route index Component={AllPosts} />
-            <Route path=':id' element={<PostDetail />} />
-            <Route path='create' element={<CreatePost />} />
+    <PostsContext.Provider value={{ posts, setPosts, categories }}>
+      <BrowserRouter>
+        <Routes>
+          <Route element={<DefaultLayout />}>
+            <Route path="/" element={<Home />} />
+            <Route path='/about' element={<About />} />
+            <Route path='/posts'>
+              <Route index Component={AllPosts} />
+              <Route path=':id' element={<PostDetail />} />
+              <Route path='create' element={<CreatePost />} />
+            </Route>
           </Route>
-        </Route>
-        <Route element={<BlankLayout />}>
-            <Route path='*' Component={NotFound} ></Route>
-          </Route>
-      </Routes>
-    </BrowserRouter>
+          <Route element={<BlankLayout />}>
+              <Route path='*' Component={NotFound} ></Route>
+            </Route>
+        </Routes>
+      </BrowserRouter>
+    </PostsContext.Provider>
   );
 };
 
